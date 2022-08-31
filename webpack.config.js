f3a74ab4da7f9bb1053b68dd48e9f6279c6fd64a
Fayaz <fayazsalim@gmail.com>
2022-08-31 21:40:54 +0530

    Base config

---
+++ webpack.config.js
@@ -0,0 +1,107 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+// plugins
+const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
+function resolvePathsFromTsConfig({
+    tsconfigPath = './tsconfig.json',
+    webpackConfigBasePath = __dirname,
+} = {}) {
+    console.log('here');
+    console.log(webpackConfigBasePath);
+    const { paths, baseUrl } = require(tsconfigPath).compilerOptions;
+
+    console.log({ paths: paths });
+
+    const aliases = {};
+
+    Object.keys(paths).forEach((item) => {
+        const key = item.replace('/*', '');
+        const value = paths[item].map((pattern) =>
+            path.resolve(
+                webpackConfigBasePath,
+                baseUrl,
+                pattern.replace('/*', '').replace('*', '')
+            )
+        );
+
+        aliases[key] = value;
+    });
+
+    return aliases;
+}
+
+const ROOT_DIR = process.cwd();
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    devServer: {
+        port: 3000,
+
+        proxy: {
+            '/api': {
+                secure: false,
+                changeOrigin: true,
+                cookieDomainRewrite: 'localhost',
+                target: 'http://localhost:9000',
+                pathRewrite: { '^/api': '' },
+            },
+        },
+    },
+
+    devtool: 'source-map',
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    { loader: 'css-loader', options: { modules: true } },
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        alias: resolvePathsFromTsConfig({
+            tsconfigPath: path.resolve(ROOT_DIR, 'tsconfig.json'),
+            webpackConfigBasePath: path.resolve(ROOT_DIR),
+        }),
+        extensions: ['.tsx', '.ts', '.js', '.scss', '.css'],
+
+        // fallback: {
+        //     https: false,
+        // },
+    },
+    plugins: [
+        new ESLintPlugin({
+            fix: true,
+            formatter: 'html',
+            extensions: ['js', 'jsx', 'ts', 'tsx'],
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+        }),
+        new NodePolyfillPlugin(),
+    ],
+};
